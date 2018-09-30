374dc16b8ed3444b0cfc4b5e9b2bf431cafc4e74
macbook MPR <jeremym@macbooks-macbook-pro.local>
2018-09-30 16:47:07 -0400

    Adds button

--- LearnGit/ViewController.swift
+++ LearnGit/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    @IBAction func buttonClicked(_ sender: UIButton)
+    {
+        print("This is a git tutorial")
     }
 
     override func didReceiveMemoryWarning() {
