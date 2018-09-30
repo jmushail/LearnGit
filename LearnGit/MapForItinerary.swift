eb61f169e0842cac595033b1be40cac12035a007
macbook MPR <jeremym@macbooks-macbook-pro.local>
2018-09-30 16:54:57 -0400

    map_branch

---
+++ LearnGit/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  LearnGit
+//
+//  Created by Jeremy Mushailov on 9/30/18.
+//  Copyright © 2018 MPR. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
